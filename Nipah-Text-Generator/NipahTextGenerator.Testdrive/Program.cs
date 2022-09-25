a8c1f536c8a9852cc09fae1056ff73030eb6cb3b
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2022-09-25 18:41:52 -0300

    adding basis for trainer test drive

--- Nipah-Text-Generator/NipahTextGenerator.Testdrive/Program.cs
+++ Nipah-Text-Generator/NipahTextGenerator.Testdrive/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using NipahTextGenerator.Syntactics;
+using NipahTextGenerator.Syntactics.Models;
+
+Console.WriteLine("Hello Syntax Trainer!");
+
+Console.WriteLine("Please type your text:");
+
+string file = Console.ReadLine() ?? "";
+
+if (file is null or "")
+    return;
+
+string text = File.ReadAllText(file);
+
+var syntax = new Syntax("en-us");
+
+var trainer = new SyntaxTrainer();
+
+trainer.Train(text, syntax);
+
+Console.WriteLine("See your trained syntax:");
+
+Console.WriteLine(syntax);
+
+Console.ReadKey(true);
