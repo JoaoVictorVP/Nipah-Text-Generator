80438aec5d532c69dd46bded8188d90c9f9c1d6c
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2022-09-25 18:41:52 -0300

    adding tokenizer, and train method to start training

--- Nipah-Text-Generator/NipahTextGenerator/Syntactics/Models/Syntax.cs
+++ Nipah-Text-Generator/NipahTextGenerator/Syntactics/Models/Syntax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NipahTokenizer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,8 @@ namespace NipahTextGenerator.Syntactics.Models;
 public class Syntax
 {
     public string Language { get; set; }
+    readonly Tokenizer tokenizer;
+    readonly TokenizerOptions tokenizerOptions;
     readonly List<SyntaxWord> words = new(32);
     readonly Dictionary<string, SyntaxWord> indexedWords = new(32);
 
@@ -26,10 +29,32 @@ public class Syntax
     public Syntax(string language)
     {
         Language = language;
+
+        tokenizer = new();
+        tokenizerOptions = new(
+            TokenizerOptions.DefaultSeparators,
+            Array.Empty<Scope>(),
+            TokenizerOptions.DefaultEndOfLines,
+            TokenizerOptions.DefaultAggregators
+        );
     }
 
-    public void Train()
+    public void Train(string text)
     {
+        var tokens = tokenizer.Tokenize(text, tokenizerOptions);
 
+        for (int i = 0; i < tokens.Count; i++)
+        {
+            Token token = tokens[i];
+            if (token.Type is TokenType.EOF) continue;
+
+            Token? before = i is 0 ? (Nullable<Token>)null : tokens[i - 1];
+            Token? next = i >= tokens.Count ? (Nullable<Token>)null : tokens[i + 1];
+
+            var word = GetEntry(token.Text);
+
+
+
+        }
     }
 }
