c30d4f31fb3e246152c56a495ac2439206a65399
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2022-09-25 18:41:52 -0300

    moving train to syntax train

--- Nipah-Text-Generator/NipahTextGenerator/Syntactics/Models/Syntax.cs
+++ Nipah-Text-Generator/NipahTextGenerator/Syntactics/Models/Syntax.cs
@@ -10,12 +10,10 @@ namespace NipahTextGenerator.Syntactics.Models;
 public class Syntax
 {
     public string Language { get; set; }
-    readonly Tokenizer tokenizer;
-    readonly TokenizerOptions tokenizerOptions;
     readonly List<SyntaxWord> words = new(32);
     readonly Dictionary<string, SyntaxWord> indexedWords = new(32);
 
-    SyntaxWord GetEntry(string word)
+    public SyntaxWord GetEntry(string word)
     {
         if(indexedWords.TryGetValue(word, out SyntaxWord? result) is false || result is null)
         {
@@ -29,32 +27,5 @@ public class Syntax
     public Syntax(string language)
     {
         Language = language;
-
-        tokenizer = new();
-        tokenizerOptions = new(
-            TokenizerOptions.DefaultSeparators,
-            Array.Empty<Scope>(),
-            TokenizerOptions.DefaultEndOfLines,
-            TokenizerOptions.DefaultAggregators
-        );
-    }
-
-    public void Train(string text)
-    {
-        var tokens = tokenizer.Tokenize(text, tokenizerOptions);
-
-        for (int i = 0; i < tokens.Count; i++)
-        {
-            Token token = tokens[i];
-            if (token.Type is TokenType.EOF) continue;
-
-            Token? before = i is 0 ? (Nullable<Token>)null : tokens[i - 1];
-            Token? next = i >= tokens.Count ? (Nullable<Token>)null : tokens[i + 1];
-
-            var word = GetEntry(token.Text);
-
-
-
-        }
     }
 }
--- Nipah-Text-Generator/NipahTextGenerator/Syntactics/SyntaxTrainer.cs
+++ Nipah-Text-Generator/NipahTextGenerator/Syntactics/SyntaxTrainer.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NipahTokenizer;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,35 @@ namespace NipahTextGenerator.Syntactics;
 
 public class SyntaxTrainer
 {
+    readonly Tokenizer tokenizer;
+    readonly TokenizerOptions tokenizerOptions;
 
+    public SyntaxTrainer()
+    {
+        tokenizer = new();
+        tokenizerOptions = new(
+            TokenizerOptions.DefaultSeparators,
+            Array.Empty<Scope>(),
+            TokenizerOptions.DefaultEndOfLines,
+            TokenizerOptions.DefaultAggregators
+        );
+    }
+    public void Train(string text)
+    {
+        var tokens = tokenizer.Tokenize(text, tokenizerOptions);
+
+        for (int i = 0; i < tokens.Count; i++)
+        {
+            Token token = tokens[i];
+            if (token.Type is TokenType.EOF) continue;
+
+            Token? before = i is 0 ? (Nullable<Token>)null : tokens[i - 1];
+            Token? next = i >= tokens.Count ? (Nullable<Token>)null : tokens[i + 1];
+
+            var word = GetEntry(token.Text);
+
+
+
+        }
+    }
 }
