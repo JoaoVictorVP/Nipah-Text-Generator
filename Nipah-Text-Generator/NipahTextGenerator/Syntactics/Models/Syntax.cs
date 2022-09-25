c0bff2b224991cc83405f7e649b6e701e5388076
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2022-09-25 18:41:51 -0300

    support for indexing of words

--- Nipah-Text-Generator/NipahTextGenerator/Syntactics/Models/Syntax.cs
+++ Nipah-Text-Generator/NipahTextGenerator/Syntactics/Models/Syntax.cs
@@ -9,5 +9,27 @@ namespace NipahTextGenerator.Syntactics.Models;
 public class Syntax
 {
     public string Language { get; set; }
+    List<SyntaxWord> words = new(32);
+    Dictionary<string, SyntaxWord> indexedWords = new(32);
 
+    SyntaxWord GetEntry(string word)
+    {
+        if(indexedWords.TryGetValue(word, out SyntaxWord result) is false)
+        {
+            result = new SyntaxWord(word);
+            words.Add(result);
+            indexedWords[word] = result;
+        }
+        return result;
+    }
+
+    public Syntax(string language)
+    {
+        Language = language;
+    }
+
+    public void Train()
+    {
+
+    }
 }
--- Nipah-Text-Generator/NipahTextGenerator/Syntactics/Models/SyntaxWord.cs
+++ Nipah-Text-Generator/NipahTextGenerator/Syntactics/Models/SyntaxWord.cs
@@ -2,6 +2,12 @@
 
 public class SyntaxWord
 {
-    public string Word { get; set; } = "";
+    public readonly string Word;
+
+    public SyntaxWord(string word)
+    {
+        Word = word;
+    }
+
     public List<SyntaxRelations> Relations { get; set; } = new(32);
 }
