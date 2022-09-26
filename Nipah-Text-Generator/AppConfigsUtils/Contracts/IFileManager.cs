8164d1d5fae8f6bb427ca5e301d69eb591e568c8
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2022-09-26 02:08:49 -0300

    implementing file manager contract and impl for system file manager

---
+++ Nipah-Text-Generator/AppConfigsUtils/Contracts/IFileManager.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConfigsUtils.Contracts;
+
+public interface IFileManager
+{
+    bool Exists(string file);
+    Stream Open(string file, FileMode mode);
+    void Write(string file, Stream stream);
+    ValueTask WriteAsync(string file, Stream stream, CancellationToken ct = default);
+    void Delete(string file);
+    ValueTask DeleteAsync(string file, CancellationToken ct = default);
+}
---
+++ Nipah-Text-Generator/AppConfigsUtils/Impl/SystemFileManager.cs
@@ -0,0 +1,43 @@
+﻿using AppConfigsUtils.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppConfigsUtils.Impl;
+
+public class SystemFileManager : IFileManager
+{
+    public bool Exists(string file) => File.Exists(file);
+
+    public Stream Open(string file, FileMode mode)
+    {
+        return File.Open(file, mode);
+    }
+
+    public void Write(string file, Stream stream)
+    {
+        var handle = File.OpenWrite(file);
+        handle.Position = 0;
+        stream.CopyTo(handle);
+    }
+
+    public async ValueTask WriteAsync(string file, Stream stream, CancellationToken ct = default)
+    {
+        var handle = File.OpenWrite(file);
+        handle.Position = 0;
+        await stream.CopyToAsync(handle, ct);
+    }
+
+    public void Delete(string file)
+    {
+        File.Delete(file);
+    }
+
+    public ValueTask DeleteAsync(string file, CancellationToken ct = default)
+    {
+        File.Delete(file);
+        return ValueTask.CompletedTask;
+    }
+}
