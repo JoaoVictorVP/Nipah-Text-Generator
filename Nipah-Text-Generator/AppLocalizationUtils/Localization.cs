9e18e19da6099930cf9f64d4d19093317807a29f
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2022-09-26 04:13:29 -0300

    adding basic support for localization and text hooking with smart track of hooks
    all tests passing

---
+++ Nipah-Text-Generator/AppLocalizationUtils/Localization.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace AppLocalizationUtils;
+
+public class Localization
+{
+    readonly Dictionary<string, string> entries = new(32);
+
+    Localization(Dictionary<string, string> entries) => this.entries = entries;
+
+    public static Localization Load(string json)
+    {
+        var entries = JsonSerializer.Deserialize<Dictionary<string, string>>(json) ?? new(32);
+        return new(entries);
+    }
+
+    public string Get(string key, string defaultValue = "")
+        => entries.TryGetValue(key, out var value)
+        ? value
+        : defaultValue;
+}
---
+++ Nipah-Text-Generator/AppLocalizationUtils/LocalizationManager.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLocalizationUtils;
+
+public class LocalizationManager
+{
+    readonly Dictionary<string, Localization> localizations = new(32);
+    Localization? current;
+
+    public event Action<Localization>? OnCurrentChanged;
+
+    public LocalizationManager()
+    {
+        OnCurrentChanged += changedTo =>
+        {
+            ExecuteHooks(changedTo);
+        };
+    }
+
+    public void Add(string language, Localization localization)
+    {
+        localizations[language] = localization;
+        if (current is null)
+            SetCurrent(language);
+    }
+
+    public void SetCurrent(string language)
+    {
+        var oldCurrent = current;
+        current = Get(language) ?? current;
+        if(oldCurrent != current && current is not null)
+            OnCurrentChanged?.Invoke(current);
+    }
+
+    public Localization? Get(string language)
+        => localizations.TryGetValue(language, out var loc)
+        ? loc
+        : null;
+
+    readonly List<WeakReference<LocalizationHook>> hooks = new(32);
+    public LocalizationHook Hook(string entry, Action<string> localizedTextCallback)
+    {
+        var result = current?.Get(entry);
+        if (result is not null)
+            localizedTextCallback(result);
+        var hook = new LocalizationHook(entry, localizedTextCallback);
+        hooks.Add(new(hook));
+        return hook;
+    }
+    void ExecuteHooks(Localization localization)
+    {
+        for (int i = 0; i < hooks.Count; i++)
+        {
+            WeakReference<LocalizationHook>? maybeHook = hooks[i];
+            if (maybeHook.TryGetTarget(out var hook))
+                hook.LocalizedTextCallback(localization.Get(hook.Hooking));
+            else
+                hooks.RemoveAt(i);
+        }
+    }
+
+    public record class LocalizationHook(string Hooking, Action<string> LocalizedTextCallback);
+}
