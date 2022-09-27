d658e820ce5ea0bc6dbe369fc2751d1b785c5f08
JoaoVictorVP - Nipah <joaovitorsl2012@hotmail.com>
2022-09-27 03:44:57 -0300

    implementing scrap pdf

--- Nipah-Text-Generator/Tools/NipahTextTools.Cli/Commands/ScrapPDFCommand.cs
+++ Nipah-Text-Generator/Tools/NipahTextTools.Cli/Commands/ScrapPDFCommand.cs
@@ -13,41 +13,47 @@ namespace NipahTextTools.Cli.Commands;
 
 public class ScrapPDFCommand : Command<ScrapPDFSettings>
 {
-    StringBuilder documentSB = new(3200);
+    readonly StringBuilder documentSB = new(3200);
     public override int Execute([NotNull] CommandContext context, [NotNull] ScrapPDFSettings settings)
     {
         documentSB.Clear();
         AnsiConsole.MarkupLine($"Scrapping pdf [bold yellow]'{settings.File}'[/] to [bold yellow]'{settings.Output}'[/]");
-        using var doc = PdfDocument.Open(settings.File);
+        using var doc = PdfDocument.Open(settings.File, new() { UseLenientParsing = false });
         foreach(var page in doc.GetPages())
         {
             AnsiConsole.MarkupLine($"Scrapping page [bold]{page.Number}[/]");
             string text = Process(page.Text);
             documentSB.AppendLine(text);
         }
-        File.WriteAllText(documentSB.ToString(), settings.Output);
+        File.WriteAllText(settings.Output, documentSB.ToString());
         AnsiConsole.MarkupLine("Done!");
         return 0;
     }
 
-    StringBuilder processSB = new(320);
+    readonly StringBuilder processSB = new(320);
     string Process(string text)
     {
         processSB.Clear();
         bool canPlaceNewLine = false;
         foreach(var c in text)
         {
-            if (c is '.' or ';' or ':' or '?' or '!' or ')')
+            if (c is '.' or ';' or ':' or '?' or '!' or ')' || char.IsUpper(c))
                 canPlaceNewLine = true;
 
-            if (c is '\n' && canPlaceNewLine is false)
-                continue;
+            if (c is '\n')
+            {
+                if (canPlaceNewLine is false)
+                    continue;
+                else
+                    canPlaceNewLine = false;
+            }
 
             if (c is '\r')
                 continue;
 
             processSB.Append(c);
         }
+
 
         return processSB.ToString();
     }
